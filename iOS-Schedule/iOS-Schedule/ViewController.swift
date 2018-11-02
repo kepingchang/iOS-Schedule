58a486d4edf0542edd8a45e5eebaf2f628863ed1
kepingchang <kepingchang@strongbrain.cn>
2018-11-02 15:45:30 +0800

    init

---
+++ iOS-Schedule/iOS-Schedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Schedule
+//
+//  Created by 柯平常 on 2018/11/2.
+//  Copyright © 2018 柯平常. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
